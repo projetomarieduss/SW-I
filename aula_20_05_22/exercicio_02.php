1981a36490401e395de4cf21c0ab917c1dbfc4fd
Maria Eduarda S. Silva <79859389+projetomarieduss@users.noreply.github.com>
2022-05-27 11:16:20 -0300

    Update exercicio_02.php

--- aula_20_05_22/exercicio_02.php
+++ aula_20_05_22/exercicio_02.php
@@ -1,3 +1,10 @@
-<?php 
+<?php
+$vetor = [];
+
+for($a=0; $a<5; $a++){
+    $sorte= rand(1,10);
+    $vetor[$a] = $sorte;
+    echo "O número $vetor[$a] foi sorteado na posição $a do vetor. <br>";
+};
 
 ?>
