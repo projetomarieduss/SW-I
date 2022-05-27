b47f228ea702604f4126982f5581d8e5c9722f5d
Maria Eduarda S. Silva <79859389+projetomarieduss@users.noreply.github.com>
2022-05-27 11:16:43 -0300

    Update exercicio_03.php

--- aula_20_05_22/exercicio_03.php
+++ aula_20_05_22/exercicio_03.php
@@ -1,4 +1,14 @@
-<?php 
-$vetor = rand(1,100);
-echo $vetor;
+<?php
+$vetor = [];
+$soma = 0;
+
+for($a=0; $a<10; $a++){
+    $sorte= rand(1,100);
+    $vetor[$a] = $sorte;
+    echo "O número $vetor[$a] foi sorteado na posição $a do vetor. <br>";
+    $soma = $soma + $vetor[$a];
+};
+$media = $soma/10;
+echo "A média de todos os valores do vetor é $media";
+
 ?>
